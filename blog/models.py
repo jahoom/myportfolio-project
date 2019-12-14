c6e36e0eff25d10123bdbb7d1914cce3c33a8f79
Jan Macioszczyk <janmac89@gmail.com>
2019-12-14 10:38:23 +0100

    Dzialajacy blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    """docstring for ."""
+    title = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='images/')
